f7f48820c7985bf643cf466ba62f0a60ccf229ed
Stace <38450525+stace94@users.noreply.github.com>
2024-01-21 17:05:05 -0500

    Update index.js

--- Back-End/index.js
+++ Back-End/index.js
@@ -22,7 +22,10 @@ const DB_URL = process.env.DB_URL;
 
 
 
-app.use(cors());
+app.use(cors({
+    credentials: true,
+    origin: 'https://blog-app-onch.onrender.com'
+}));
 app.use(express.json());
 app.use(cookieParser());
 app.use('/uploads', express.static(__dirname + '/uploads'));
